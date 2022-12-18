96bb003d40478f594deb7d36085bb8bbdd1a8776
Jevgenijs Kozlovs <unstoo@gmail.com>
2022-12-18 22:09:23 +0200

    feat: add cd command

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const path = require('node:path')
+const { promises: Fs } = require('node:fs')
+const os = require('node:os')
 
-const startingDir = process.cwd();
-let username;
+const startingDir = os.homedir();
+let username = 'Anonymous';
 let currentDir = startingDir;
 
 const getMsg = {
@@ -18,14 +20,25 @@ const commands = {
     process.exit(0);
   },
   ['up']: () => {
-    goUp();
-    console.log(getMsg.cwd());
+    currentDir = path.resolve(currentDir, '..');
   },
-  ['']: () => { },
+  ['cd']: async (argPath) => {
+    const maybeDir = path.resolve(currentDir, argPath);
+    const stat = await Fs.stat(maybeDir);
+    const isDir = stat.isDirectory();
+    // can't cd into a file
+    // can't cd into a non-existing dir
+    if (!isDir) throw Error();
+    currentDir = maybeDir;
+  },
 };
 
-function goUp() {
-  currentDir = path.join(currentDir, '..');
+const validateArgs = {
+  ['.exit']: args => args == '',
+  ['up']: args => args == '',
+  ['cd']: (args) => {
+    return args !== '' && typeof args === 'string';
+  },
 };
 
 function start() {
@@ -40,16 +53,18 @@ function start() {
     try {
       cmd = parseCmd(input)
     } catch (err) {
+      console.error(err)
       console.log(getMsg.errInput());
-      return;
     }
 
     try {
-      cmd()
+      cmd && await cmd()
     } catch (err) {
       console.error(err)
       console.log(getMsg.errOperation());
     }
+
+    console.log(getMsg.cwd());
   });
 
   process.on('SIGINT', () => {
@@ -58,24 +73,21 @@ function start() {
   });
 }
 
+
+function parseCmd(str) {
+  const name = Object.keys(commands).find(cmd => str.startsWith(cmd));
+  if (!name) throw Error();
+
+  const inputArgs = str.slice(name.length).trim();
+  if (!validateArgs[name](inputArgs)) throw Error();
+
+  return commands[name].bind({}, inputArgs);
+}
+
 function getUsername() {
   const args = process.argv.slice(2);
   const nameArg = args.find(arg => arg.startsWith('--username='));
   return nameArg.slice('--username='.length);
-}
-
-function parseCmd(str) {
-  const tokens = str
-    .trim()
-    .split(' ')
-    .map(strChunk => strChunk.replace(/\s/g, ''))
-    .filter(token => token);
-  const cmdName = tokens[0];
-  const cmdArgs = tokens.slice(1);
-
-  if (!commands[cmdName]) throw Error();
-
-  return commands[cmdName];
 }
 
 start()
